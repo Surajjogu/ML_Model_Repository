670cf28cef534f2a0b15409d49016c60dea7bb08
Surajjogu <160999951+Surajjogu@users.noreply.github.com>
2024-02-26 17:40:07 +0530

    Update app.py

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ model = pickle.load(open('model.pkl', 'rb'))
 
 @app.route('/')
 def home():
-    return render_template('index.html')
+    return render_template('https://raw.githubusercontent.com/Surajjogu/ML_Model_Repository/main/index.html')
 
 @app.route('/predict',methods=['POST'])
 def predict():
