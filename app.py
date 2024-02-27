85b3cda3f47014d8e10b5692f1273382a9886944
Surajjogu <160999951+Surajjogu@users.noreply.github.com>
2024-02-27 15:40:43 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,32 +1,16 @@
 # -*- coding: utf-8 -*-
 """
-Created on Tue Feb 27 12:14:05 2024
+Created on Mon Feb 26 11:51:37 2024
 
 @author: kalpavruksh_sjo
 """
-
-from flask import Flask, request, render_template, redirect, url_for
-import numpy as np
-import pickle
+from flask import Flask
 
 app = Flask(__name__)
-# model = pickle.load(open('https://raw.githubusercontent.com/Surajjogu/ML_Model_Repository/main/model.pkl', 'rb'))
 
 @app.route('/')
 def index():
-    return render_template('https://raw.githubusercontent.com/Surajjogu/ML_Model_Repository/main/index.html')
+    return "<center><h1>Flask app deployment of Azure</h1></center>"
 
-
-"""
-@app.route('/predict',methods=['POST'])
-def predict():
-    features = [int(x) for x in request.form.values()]
-    final_features = [np.array(features)]
-    prediction = model.predict(final_features)
-    
-    output = prediction[0]
-    
-    return render_template('https://raw.githubusercontent.com/Surajjogu/ML_Model_Repository/main/index.html', prediction_text='Loan Eligibility = {}'.format(output))
-"""
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run()
